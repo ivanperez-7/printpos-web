3b1445e4ffadad8e8248d6176b363b382b2630ee
ivanperez-7 <ivan.perez27@outlook.com>
2025-12-11 16:19:57 -0600

    Quita strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider, createRouteMask, createRouter } from '@tanstack/react-router';
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ThemeProvider } from './components/theme-provider.tsx';
@@ -37,10 +36,8 @@ const rootElement = document.getElementById('app');
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
-      <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </StrictMode>
+    <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   );
 }
